5e1f5175f6692531ffd4ccfe0038300a43133457
soujanya131narlapuram <soujanya131narlapuram@gmail.com>
2022-11-30 23:28:40 +0530

    UC2 push

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
 public class TicTacToeGame {
-   public static char[] uc1() {
-        char[] arr={' ',' ',' ',' ',' ',' ',' ',' ',' ',' '};
-        return arr;
+    static  char[] board = {' ',' ',' ',' ',' ',' ',' ',' ',' ',' '};
+    static int player=0;
+    public static void emptyBoard() {
+        System.out.println("Empty Board created");
+        return;
     }
-            public static void main(String[] args){
-              char[] tictactoearr=  uc1();
-            }
-        }
+    public static void playerSelection(){
+        System.out.println("Enter 1 for Player1 and 2 for player2");
+        Scanner sc=new Scanner(System.in);
+        player=sc.nextInt();
+        System.out.println("Inputting x for player1 and o for player2");
+    }
+    public static void main(String[] args){
+        emptyBoard();
+        playerSelection();
+    }
+}
 
